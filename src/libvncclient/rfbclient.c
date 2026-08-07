19df7871c3a3e0a59cc4f70e3987f0f6eaab477e
Gaurav Ujjwal <gujjwal00@gmail.com>
2026-08-07 05:39:02 +0530

    Revert "libvncclient: Don't ignore screen size updates with screen.id = 0"
    This reverts commit 49ebce05a956adfdff90017c71500270509e1930.

--- src/libvncclient/rfbclient.c
+++ src/libvncclient/rfbclient.c
@@ -2191,7 +2191,7 @@ HandleRFBServerMessage(rfbClient* client)
           if (!ReadFromRFBServer(client, ((char *)&screen), sz_rfbExtDesktopScreen)) {
             return FALSE;
           }
-          if (screen.width && screen.height) {
+          if (screen.id != 0 && screen.width && screen.height) {
             client->screen = screen;
           } else {
             invalidScreen = TRUE;
